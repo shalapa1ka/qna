5de5dff557959cbe4af60fb4540fb3b0b8287b65
shalapa1ka <flecks07@gmail.com>
2022-05-24 16:28:25 +0300

    Create answer and question models
    Created tests, migrations and models for answer and question with BDD
    process.

---
+++ spec/models/questions_spec.rb
@@ -0,0 +1,9 @@
+require 'rails_helper'
+
+describe Question, type: :model do
+  context 'validates' do
+    it { should validate_presence_of :title }
+    it { should validate_presence_of :body }
+    it { should have_many(:answers).dependent(:destroy) }
+  end
+end
