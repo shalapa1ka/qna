fc6b2956f2838511900209b6da028080fabef9f4
shalapa1ka <flecks07@gmail.com>
2022-05-30 20:13:02 +0300

    Refactor migrations

--- db/migrate/20220524130003_create_answers.rb
+++ db/migrate/20220524130003_create_answers.rb
@@ -1,9 +1,10 @@
 class CreateAnswers < ActiveRecord::Migration[7.0]
   def change
     create_table :answers do |t|
-      t.string :title
       t.string :body
       t.timestamps
+      t.references :question, index: true
+      t.references :user, index: true
     end
   end
 end
