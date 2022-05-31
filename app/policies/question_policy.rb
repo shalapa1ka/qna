eb884afe5f4a6410e80349f039cfab5ae9274af0
shalapa1ka <flecks07@gmail.com>
2022-05-31 18:32:42 +0300

    Add authorization.
    Added and configured Pundit.

---
+++ app/policies/question_policy.rb
@@ -0,0 +1,21 @@
+class QuestionPolicy < ApplicationPolicy
+  def create?
+    user.present?
+  end
+
+  def update?
+    user.admin? || user.author?(record)
+  end
+
+  def destroy?
+    user.admin? || user.author?(record)
+  end
+
+  def index?
+    true
+  end
+
+  def show?
+    true
+  end
+end
